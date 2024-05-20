bf1bb7ab5e5f6a454d2e70ea3093d5d3592362bd
sasikiran0 <sasikran@bloomitsolution.in>
2024-05-20 15:55:44 +0530

    fix cors error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const db = require("./db");
 const authRoute = require("./routes/authRoute");
@@ -15,6 +16,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server is healthy!");
